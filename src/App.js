c18032dd55954f0d947a672397fb133f9d69704d
Albert Nutifafa Dovlo <albertnutifafa@gmail.com>
2022-06-25 18:12:45 +0000

    creating routefor home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Login from "./components/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </Router>
   );
